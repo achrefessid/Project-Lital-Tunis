af61458972c97db6fd3a3b28048d5f64c5956551
Achref <Essid>
2020-07-01 08:25:49 +0100

    modify user

--- src/components/ManagingUser/vueUser.js
+++ src/components/ManagingUser/vueUser.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-import { Icon, Label, Menu, Table, Dropdown } from "semantic-ui-react";
+import {
+  Button as Btn,
+  Icon,
+  Label,
+  Menu,
+  Table,
+  Dropdown,
+} from "semantic-ui-react";
 import { Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import ModalEdite from "./EditeUser";
 import {
@@ -17,23 +24,27 @@ class VueUser extends Component {
   render() {
     return (
       <div className="tab">
-        <Link to="/signin">
-        <Dropdown
-          text="Ajouter un utilisateur"
-          icon="add user"
-          floating
-          labeled
-          button
-          className="icon"
-        ></Dropdown>
-        </Link>
+        <NavLink exact to="/signin">
+          <Btn icon labelPosition="left">
+            <Icon name="add user" />
+            Ajouter un utilisateur
+          </Btn>
+        </NavLink>
+        {/* <NavLink exact to="/signin">
+          <Dropdown
+            text="Ajouter un utilisateur"
+            icon="add user"
+            floating
+            labeled
+            button
+            className="icon"
+          ></Dropdown>
+        </NavLink> */}
         <Table celled>
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>Pseudo</Table.HeaderCell>
               <Table.HeaderCell>Mot de passe</Table.HeaderCell>
-
-              
               <Table.HeaderCell className="edit">Editer</Table.HeaderCell>
               <Table.HeaderCell>Supprimer</Table.HeaderCell>
             </Table.Row>
@@ -65,19 +76,7 @@ class VueUser extends Component {
           </Table.Body>
 
           <Table.Footer>
-            <Table.Row>
-              <Table.HeaderCell colSpan="4">
-                <Menu floated="right" pagination>
-                  <Menu.Item as="a" icon>
-                    <Icon name="chevron left" />
-                  </Menu.Item>
-                  <Menu.Item as="a">1</Menu.Item>
-                  <Menu.Item as="a" icon>
-                    <Icon name="chevron right" />
-                  </Menu.Item>
-                </Menu>
-              </Table.HeaderCell>
-            </Table.Row>
+            
           </Table.Footer>
         </Table>
       </div>
